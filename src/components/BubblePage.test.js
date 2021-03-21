03d67d950070aad1b66b6d5886b5a50c257122cd
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-03-21 19:52:20 -0400

    Implemented the two tests for BubblePage and Bubbles Component

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,101 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+import Bubbles from "./Bubbles";
+import axios from 'axios';
+import { axiosWithAuth } from "../helpers/axiosWithAuth.js";
+
+const mockColorsData = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff",
+    },
+    id: 1,
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc",
+    },
+    id: 2,
+  },
+  {
+    color: "aqua",
+    code: {
+      hex: "#00ffff",
+    },
+    id: 3,
+  },
+  {
+    color: "aquamarine",
+    code: {
+      hex: "#7fffd4",
+    },
+    id: 4,
+  },
+  {
+    color: "lilac",
+    code: {
+      hex: "#9a99dd",
+    },
+    id: 5,
+  },
+  {
+    color: "softpink",
+    code: {
+      hex: "#dd99ba",
+    },
+    id: 6,
+  },
+  {
+    color: "bisque",
+    code: {
+      hex: "#dd9a99",
+    },
+    id: 7,
+  },
+  {
+    color: "softyellow",
+    code: {
+      hex: "#dcdd99",
+    },
+    id: 8,
+  },
+  {
+    color: "blanchedalmond",
+    code: {
+      hex: "#ffebcd",
+    },
+    id: 9,
+  },
+  {
+    color: "blue",
+    code: {
+      hex: "#6093ca",
+    },
+    id: 10,
+  },
+  {
+    color: "blueviolet",
+    code: {
+      hex: "#8a2be2",
+    },
+    id: 11,
+  },
+]
 
 test("Renders BubblePage without errors", () => {
   // Finish this test
+  render(<BubblePage />);
 });
 
-test("Fetches data and renders the bubbles on mounting", () => {
+test("Fetches data and renders the bubbles on mounting", async () => {
   // Finish this test
+  const { rerender, getAllByTestId, queryByTestId } = render(<Bubbles colors={[]} />);
+
+  rerender(<Bubbles colors={mockColorsData} />);
+
 });
 
 //Task List
